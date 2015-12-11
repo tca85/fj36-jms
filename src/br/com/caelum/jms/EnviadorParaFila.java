e98859b230f9dfb9876579cccc73c4d00d8d3aa9
tca85 <thiago.alves.85@gmail.com>
2015-12-11 21:58:47 -0200

    envia mensagem para fila do middleware

---
+++ src/br/com/caelum/jms/EnviadorParaFila.java
@@ -0,0 +1,38 @@
+package br.com.caelum.jms;
+
+import java.util.Scanner;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * 
+ * @author tca85
+ *
+ */
+public class EnviadorParaFila {
+	
+	public static void main(String[] args) throws NamingException {
+		InitialContext ic = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) ic.lookup("jms/RemoteConnectionFactory");
+		
+		Queue queue = (Queue) ic.lookup("jms/FILA_GERADOR");
+		
+		try(JMSContext context = factory.createContext("jms", "jms2")){
+			JMSProducer producer = context.createProducer();
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				producer.send(queue, line);
+			}
+			
+			scanner.close();
+		}
+	}
+}
